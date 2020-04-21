490afb775bf82ed31510e0ab6eef17428500ded6
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-21 08:22:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,24 @@ let bigFluffyDog1 = new Dog("Buzz", "greetPyrenees");
 let bigFluffyDog2 = new Dog("Woody", "labrador");
 bigFluffyDog1;
 bigFluffyDog2;
+
+class Breakfast {
+  constructor(food, drink){
+    this.food = food;
+    this.drink = drink;
+  }
+}
+
+class Lunch {
+  constructor(salad, soup, drink){
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert){
+    
+  }
+}
