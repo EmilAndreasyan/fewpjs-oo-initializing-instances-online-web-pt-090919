926ae8ab1d20313e7ee39ac506f7d2a0e5ae1afa
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-04-21 08:23:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -28,6 +28,9 @@ class Lunch {
 
 class Dinner {
   constructor(salad, soup, entree, dessert){
-    
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
   }
 }
